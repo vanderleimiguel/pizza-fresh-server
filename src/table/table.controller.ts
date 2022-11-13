6f62b76b1630513acce1b01dcd0542fbe7017a0f
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-11-13 16:07:47 -0300

    create table dto

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTableDto } from './dto/create-table.dto';
 import { TableService } from './table.service';
 
 // end point localhost:3333/table
@@ -15,7 +16,8 @@ export class TableController {
 
   // metodo utilizado no endpoint
   @Post()
-  create() {
-    return this.tableService.create();
+  // create ira ter um body que ira receber um createtabledto com o tipo CreateTableDto vindo do arquivo
+  create(@Body() createTableDto: CreateTableDto) {
+    return this.tableService.create(createTableDto);
   }
 }
