9b69a97b999236a4853d6b734fd1f07c9426d809
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-11-13 15:49:42 -0300

    feat table add endpoint create

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
+// end point localhost:3333/table
 @Controller('table')
 export class TableController {
+  // metodo utilizado no endpoint
   @Get()
   findAll() {
     return 'Buscar todas as mesas';
+  }
+
+  // metodo utilizado no endpoint
+  @Post()
+  create() {
+    return 'Criar uma mesa';
   }
 }
