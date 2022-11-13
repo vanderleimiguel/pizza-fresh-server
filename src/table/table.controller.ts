71a490509d45f48eb36ce01f753d1d15c5478b89
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-11-13 15:59:21 -0300

    feat table: create service

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { TableService } from './table.service';
 
 // end point localhost:3333/table
 @Controller('table')
 export class TableController {
+  //
+  constructor(private tableService: TableService) {}
+
   // metodo utilizado no endpoint
   @Get()
   findAll() {
-    return 'Buscar todas as mesas';
+    return this.tableService.findAll();
   }
 
   // metodo utilizado no endpoint
   @Post()
   create() {
-    return 'Criar uma mesa';
+    return this.tableService.create();
   }
 }
