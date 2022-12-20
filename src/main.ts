fe520d0c594075dbd9598a33f212718d11d874a1
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-12-20 11:07:20 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //validation
+  app.useGlobalPipes(new ValidationPipe({}));
 
   // Swagger
   const config = new DocumentBuilder()
--- src/table/dto/create-table.dto.ts
+++ src/table/dto/create-table.dto.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPositive } from 'class-validator';
 
 export class CreateTableDto {
+  @IsNumber()
+  @IsPositive()
   @ApiProperty({
     description: 'O numero da mesa',
     example: 1,
